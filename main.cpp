9e062beac3cca2df3331d354e6079fac040c9448
dqy <1016751306@qq.com>
2022-06-01 21:36:40 +0800

    添加了对font.h的调用

--- main.cpp
+++ main.cpp
@@ -2,97 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-//以下三个宏需修改为角度：
-#define LIFT0 A//1350 落笔写字 
-#define LIFT1 B//1250 写字时抬臂动作 一般比lift0 小100左右
-#define LIFT2 C//1100 高抬笔架
+#include <font.h>
 
-// int servoLift = LIFT2;
-
-//抬笔函数，调整笔的高度 0，落笔写字； 1，书写时抬笔； 2，太高笔用于插板擦
-// void lift(int lift)
-// {
-//     switch (lift)
-//     {
-//         // room to optimize !
-
-//     case 0:
-
-//         if (servoLift >= LIFT0)
-//         {
-//             while (servoLift >= LIFT0)
-//             {
-//                 servoLift--;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift); //向舵机函数传递角度
-//             }
-//         }
-//         else
-//         {
-//             while (servoLift <= LIFT0)
-//             {
-//                 servoLift++;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift);
-//             }
-//         }
-
-//         break;
-
-//     case 1:
-
-//         if (servoLift >= LIFT1)
-//         {
-//             while (servoLift >= LIFT1)
-//             {
-//                 servoLift--;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift);
-//             }
-//         }
-//         else
-//         {
-//             while (servoLift <= LIFT1)
-//             {
-//                 servoLift++;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift);
-//             }
-//         }
-
-//         break;
-
-//     case 2:
-
-//         if (servoLift >= LIFT2)
-//         {
-//             while (servoLift >= LIFT2)
-//             {
-//                 servoLift--;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift);
-//             }
-//         }
-//         else
-//         {
-//             while (servoLift <= LIFT2)
-//             {
-//                 servoLift++;
-//                 //servo1.writeMicroseconds(servoLift);
-//                 //delayMicroseconds(LIFTSPEED);
-//                 duoji(servoLift);
-//             }
-//         }
-//         break;
-//     }
-// }
-
-void voice_broadcast(void)
+void voice_broadcast(int * h_m)
 {
     char s[20] = "play ##:##.mp3"; // 系统命令
     int hour, minute;
@@ -102,7 +14,11 @@ void voice_broadcast(void)
     time(&tmpcal_ptr);
     tmp_ptr = gmtime(&tmpcal_ptr);
     hour = tmp_ptr->tm_hour + 8; // 时区相差八小时
+    h_m[0] = hour / 10;
+    h_m[1] = hour % 10; // 将当前系统时间的小时的十位和个位拆分
     minute = tmp_ptr->tm_min;
+    h_m[2] = minute / 10;
+    h_m[3] = minute % 10; // 将当前系统时间的分钟的十位和个位拆分
     // 获取当前时间并分离小时和分钟
 
     if (hour > 12)
@@ -126,12 +42,23 @@ void voice_broadcast(void)
     itoa(minute / 10, s + 8, 10);
     itoa(minute % 10, s + 9, 10);
     s[10] = '.'; // 写入分钟个位后导致s[10]='\0'
-    puts(s);
+    system(s);
 }
 
 int main()
 {
-    voice_broadcast(); //语音播报
-    // write();
-    // clean();
+    int i, h_m[4] = {0}; // h_m数组分别存放当前系统时间小时的十位、个位以及分钟的十位、个位
+    int flag = 0; // flag代表组成一个数字的各组坐标的x坐标
+    voice_broadcast(h_m); //语音播报
+
+    for (i = 0; i < 4; i++)
+    {
+        while (LC_Num0[h_m[i]][flag] != 0)
+        {
+            readl(h_m[i], flag, i + 1); // i + 1分别等于1, 2, 3, 4,对应四个数字
+            // 执行结束后得到坐标(Tx, Ty)
+            number(point.Tx, point.Ty, h_m[i], 0.9);
+            flag++;
+        }
+    }
 }
